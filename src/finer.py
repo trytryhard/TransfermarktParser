3f9bbbc10db47e0c4b2478a6f6f06df048c21bd9
vtitov <vtduch@gmail.com>
2025-12-14 23:23:37 +0200

    Add linters to automatically check code

--- src/finer.py
+++ src/finer.py
@@ -1,25 +1,34 @@
 import re
 
-def linkFiner(val:str)->tuple:
-    '''
+
+def linkFiner(val: str) -> tuple:
+    """
     :param val: string with club id on transfermarkt
     :return: corrected link with last actual squad \ raise error
-    '''
+    """
     try:
-        val = re.findall(r'\d+',val)[0]
-        return ('https://www.transfermarkt.com/defualt/startseite/verein/'+val, val)
+        val = re.findall(r"\d+", val)[0]
+        return ("https://www.transfermarkt.com/defualt/startseite/verein/" + val, val)
     except:
-        print(f'Your input: {val}\n',
-              'It should contains digit-identificator of club',
-              r'like this: https://www.transfermarkt.com/sokol-saratov/startseite/verein/3834 \n')
-        raise NameError('Wrong team link!')
+        print(
+            f"Your input: {val}\n",
+            "It should contains digit-identificator of club",
+            r"like this: https://www.transfermarkt.com/sokol-saratov/startseite/verein/3834 \n",
+        )
+        raise NameError("Wrong team link!")
 
-def posFiner(val:str)->str:
-    '''
+
+def posFiner(val: str) -> str:
+    """
     :param val: long-name of position
     :return res: short-name of position
-    '''
-    helpDict = {'Goalkeeper':'GK','Defender':'DEF','Midfield':'MID','Attack':'ATK'}
+    """
+    helpDict = {
+        "Goalkeeper": "GK",
+        "Defender": "DEF",
+        "Midfield": "MID",
+        "Attack": "ATK",
+    }
 
     if val in helpDict:
         return helpDict[val]
